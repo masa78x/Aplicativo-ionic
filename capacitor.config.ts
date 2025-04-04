daa20d786c10abd8c02024c21dc5b7118005781b
masa78x <marcosamaral08@live.com>
2025-04-04 12:11:42 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Aplicativo',
+  webDir: 'www'
+};
+
+export default config;
